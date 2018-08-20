053462de07f0cfb2859657ec4b1034ce78910cb0
zawuza <andreizawuza@gmail.com>
2018-08-20 15:28:01 +0200

    Development: - Work on cmd application

--- cmd/soccer.c
+++ cmd/soccer.c
@@ -2,15 +2,26 @@
 #include <winbase.h>
 #include <windef.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+bool DEBUG = true;
 
 typedef int (*ExecScript)(char *, char ***, int *);
 
 int loadSoccerLib(*ExecScript out_proc)
 {
-    HINSTANCE testLibrary = LoadLibrary("libsoccer.dll");
+    HINSTANCE testLibrary;
+    if (DEBUG)
+    {
+        testLibrary = LoadLibrary("..\\libsoccer\\_dll\\Win32\\Debug\\libsoccer.dll");
+    }
+    else
+    {
+        testLibrary = LoadLibrary("libsoccer.dll");
+    }
     if (testLibrary)
     {
-        out_ptoc = &(ExecScript)GetProcAddress(testLibrary, "ExecScript");
+        out_proc = &(ExecScript)GetProcAddress(testLibrary, "ExecScript");
         return testLibrary;
     }
     else
@@ -24,8 +35,19 @@ int unLoadLib(HINSTANCE a_lib)
     FreeLibrary(a_lib);
 }
 
-int loadScriptFromFile(char *filename; char **out_script)
+int loadScriptFromFile(char *filename, char **out_script)
 {
+    FILE *script = fopen(filename, "r");
+
+    fseek(script, 0, SEEK_END);
+    long fsize = ftell(script);
+    fseek(script, 0, SEEK_SET);
+
+    *out_script = malloc(fsize + 1);
+    fread(*out_script, fsize, 1, script);
+    fclose(script);
+
+    out_script[fsize] = 0;
 }
 
 int main(int argc, char *argv[])
@@ -41,15 +63,16 @@ int main(int argc, char *argv[])
             char *scriptFilename = argv[1];
             char *script;
             int result = loadScriptFromFile(scriptFilename, &script);
+            printf("%s", &script);
             if (result)
             {
                 char **out_array = NULL;
                 int out_length = 0;
-                _ExecScript(a_script, &out_array, &out_length);
+                _ExecScript(script, &out_array, &out_length);
             }
             else
             {
-                printf("%s","Can't load script from a file");
+                printf("%s", "Can't load script from a file");
             }
         }
         else
