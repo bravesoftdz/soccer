a9c51133b8a01b71f43725c3f101d6938b381b21
ZAwuza <andreizawuza@gmail.com>
2018-09-11 23:29:11 +0200

    Development: - Work on the lib interface

--- cmd/prot_pas/soccer.lpr
+++ cmd/prot_pas/soccer.lpr
@@ -6,14 +6,18 @@ uses
   SysUtils,
   Classes;
 
-  procedure ExecScript(AScript: PAnsiChar; var OutArray: array of PAnsiChar;
-  var OutLength: Int32); stdcall; external 'libsoccer.dll';
+type
+  TOutArray = array [0 .. 20] of PAnsiChar;
+  POutArray = ^TOutArray;
+
+  procedure ExecScript(AScript: PAnsiChar; var OutArray: POutArray; var OutLength: Int32);
+  stdcall; external 'libsoccer.dll';
 
 var
   LFileName: string;
   LStringList: TStringList;
   LScript: PAnsiChar;
-  LOutArray: array of PAnsiChar;
+  LOutArray: POutArray;
   LOutLength: Int32;
   i: integer;
 
@@ -32,7 +36,7 @@ begin
       LScript := PAnsiChar(LStringList.Text);
       ExecScript(LScript, LOutArray, LOutLength);
       for i := 0 to LOutLength - 1 do
-        Writeln(LOutArray[0] + ',');
+        Writeln(LOutArray^[0] + ',');
     except
       on E: Exception do
         Writeln(E.Message);
--- libsoccer/src/LibSoccer.Export.pas
+++ libsoccer/src/LibSoccer.Export.pas
@@ -9,25 +9,34 @@ uses
 
   Soccer.Main;
 
-procedure ExecScript(AScript: PAnsiChar; var OutArray: TArray<PAnsiChar>;
+type
+
+  TOutArray = array [0 .. 20] of PAnsiChar;
+  POutArray = ^TOutArray;
+
+procedure ExecScript(AScript: PAnsiChar; var OutArray: POutArray;
   var OutLength: Int32); stdcall;
 
 implementation
 
-procedure ExecScript(AScript: PAnsiChar; var OutArray: TArray<PAnsiChar>;
+procedure ExecScript(AScript: PAnsiChar; var OutArray: POutArray;
   var OutLength: Int32);
 var
   LSoccer: TSoccer;
   LStrList: TList<AnsiString>;
   LStr: AnsiString;
   i: integer;
+  LBorder: integer;
 begin
   LSoccer := TSoccer.Create;
   try
     try
       LStrList := LSoccer.ExecScript(AScript);
-      SetLength(OutArray, LStrList.Count);
       OutLength := LStrList.Count;
+      if LStrList.Count - 1 > 20 then
+        LBorder := 20
+      else
+        LBorder := LStrList.Count;
       for i := 0 to LStrList.Count - 1 do
       begin
         LStr := LStrList[i];
@@ -36,7 +45,6 @@ begin
     except
       on E: Exception do
       begin
-        SetLength(OutArray, 2);
         OutArray[0] := PAnsiChar(AnsiString('error'));
         OutArray[1] := PAnsiChar(AnsiString(E.ClassName + ' : ' + E.Message));
       end;
